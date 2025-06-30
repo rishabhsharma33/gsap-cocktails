077a7907aa736d1c49e86527fc80047595b4b378
Rishabh <rishabh.sharma3@appinventiv.com>
2025-06-30 15:21:08 +0530

    Implemented cocktails section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -12,6 +13,7 @@ const App = () => {
       <Hero />
       {/* For scrolling purpose for testing */}
       {/* <div className="h-dvh bg-black"></div> */}
+      <Cocktails />
     </main>
   );
 };
